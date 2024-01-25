74a691c03c712549ccfc776b4c6ecb7df827bb86
tnd18091995 <tnd18091995@gmail.com>
2024-01-25 17:15:18 +0700

    HN

---
+++ may-tinh-co-ban.js
@@ -0,0 +1,23 @@
+function num1() {
+    document.getElementById("hienthi").innerHTML ="1";
+}
+function num2() {
+    document.getElementById("hienthi").innerHTML ="2";}
+    function num3() {
+        document.getElementById("hienthi").innerHTML ="3";}
+function num4() {
+    document.getElementById("hienthi").innerHTML ="4";
+}
+function num5() {
+    document.getElementById("hienthi").innerHTML ="5";}
+function num6() {
+    document.getElementById("hienthi").innerHTML ="6";}
+function num7() {
+    document.getElementById("hienthi").innerHTML ="7";
+}
+function num8() {
+    document.getElementById("hienthi").innerHTML ="8";}
+function num9() {
+    document.getElementById("hienthi").innerHTML ="9";}
+function num0() {
+    document.getElementById("hienthi").innerHTML ="0";}
